74ca6b485a885b0d76b4d499387f471037b3656e
Juridicaribesoftware <juridicaribesoftware@gmail.com>
2024-10-08 14:26:05 -0500

    Actualizaciones de rendiemiento y seguridad

--- src/routes/login.js
+++ src/routes/login.js
@@ -10,6 +10,7 @@ router.get('/', loginControllers.listar)
 router.get('/buscar', loginControllers.listarB)
 router.post('/crear', loginControllers.agregar)
 router.get('/servidor', (req, res) => {
+    console.log('Ping Despertar')
     res.send('Servidor En EJECUCION')
 })
 router.post('/autenticacion', loginControllers.autenticacion)
